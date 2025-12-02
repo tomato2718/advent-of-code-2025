dc4a77c3d7075b5b9640acd4b8bad82853de648b
tomato2718 <qweasdzxc030@gmail.com>
2025-12-02 13:54:24 +0800

    Refactor: Fix output text

--- src/day1/run.rs
+++ src/day1/run.rs
@@ -3,9 +3,9 @@ use super::secret_entrance::SecretEntrance;
 pub fn run(raw_input: String) {
     let input: Vec<&str> = raw_input.split("\n").collect();
     println!("Day1:");
-    println!("  password: {}", SecretEntrance::find_password(&input));
+    println!("  Password: {}", SecretEntrance::find_password(&input));
     println!(
-        "  password with CLICK: {}",
+        "  Password with CLICK: {}",
         SecretEntrance::find_password_with_click(&input)
     );
 }
