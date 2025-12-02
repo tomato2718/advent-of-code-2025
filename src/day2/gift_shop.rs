caaa9cbcfc59c3fc0deea906dfb8c0f69bd5372b
tomato2718 <qweasdzxc030@gmail.com>
2025-12-02 15:02:34 +0800

    Feat: Day2 q2

--- src/day2/gift_shop.rs
+++ src/day2/gift_shop.rs
@@ -1,9 +1,19 @@
 pub struct GiftShop {}
 
 impl GiftShop {
-    pub fn add_up_invalid_ids(ids: &Vec<&str>) -> usize {
+    pub fn sum_of_id_repeated_twice(ids: &Vec<&str>) -> usize {
         GiftShop::parse_ids(ids).fold(0, |acc, (start, end)| {
-            acc + (start..=end).filter(GiftShop::is_valid_id).sum::<usize>()
+            acc + (start..=end)
+                .filter(GiftShop::repeated_twice)
+                .sum::<usize>()
+        })
+    }
+
+    pub fn sum_of_id_repeated_more_than_twice(ids: &Vec<&str>) -> usize {
+        GiftShop::parse_ids(ids).fold(0, |acc, (start, end)| {
+            acc + (start..=end)
+                .filter(GiftShop::repeated_more_than_twice)
+                .sum::<usize>()
         })
     }
 
@@ -23,9 +33,29 @@ impl GiftShop {
         })
     }
 
-    fn is_valid_id(id: &usize) -> bool {
+    fn repeated_twice(id: &usize) -> bool {
         let s = id.to_string();
         s[0..s.len() / 2] == s[s.len() / 2..]
+    }
+
+    fn repeated_more_than_twice(id: &usize) -> bool {
+        let digits = (*id as f64).log10().floor() as u32 + 1;
+        (1..=digits / 2).any(|digit| {
+            let mut id = *id;
+            let t = 10_usize.pow(digit);
+            let want = id % t;
+            if (want as f32).log10().floor() as u32 + 1 != digit {
+                return false;
+            }
+            while id > 0 {
+                let last = id % t;
+                if last != want {
+                    return false;
+                }
+                id /= t;
+            }
+            true
+        })
     }
 }
 
@@ -34,7 +64,7 @@ mod test {
     use super::*;
 
     #[test]
-    fn add_up_invalid_ids() {
+    fn sum_of_id_repeated_twice() {
         let ids = vec![
             "11-22",
             "95-115",
@@ -49,9 +79,30 @@ mod test {
             "2121212118-2121212124",
         ];
 
-        let res = GiftShop::add_up_invalid_ids(&ids);
+        let res = GiftShop::sum_of_id_repeated_twice(&ids);
 
         assert_eq!(res, 1227775554);
+    }
+
+    #[test]
+    fn sum_of_id_repeated_more_than_twice() {
+        let ids = vec![
+            "11-22",
+            "95-115",
+            "998-1012",
+            "1188511880-1188511890",
+            "222220-222224",
+            "1698522-1698528",
+            "446443-446449",
+            "38593856-38593862",
+            "565653-565659",
+            "824824821-824824827",
+            "2121212118-2121212124",
+        ];
+
+        let res = GiftShop::sum_of_id_repeated_more_than_twice(&ids);
+
+        assert_eq!(res, 4174379265);
     }
 
     #[test]
@@ -91,8 +142,18 @@ mod test {
     }
 
     #[test]
-    fn is_valid_id() {
-        assert!(GiftShop::is_valid_id(&123123));
-        assert!(!GiftShop::is_valid_id(&12321));
+    fn repeated_twice() {
+        assert!(GiftShop::repeated_twice(&123123));
+        assert!(!GiftShop::repeated_twice(&12321));
+    }
+
+    #[test]
+    fn repeated_more_than_twice() {
+        assert!(GiftShop::repeated_more_than_twice(&111));
+        assert!(GiftShop::repeated_more_than_twice(&1212));
+        assert!(GiftShop::repeated_more_than_twice(&123123));
+        assert!(!GiftShop::repeated_more_than_twice(&12321));
+        assert!(!GiftShop::repeated_more_than_twice(&123321));
+        assert!(!GiftShop::repeated_more_than_twice(&10101));
     }
 }
--- src/day2/run.rs
+++ src/day2/run.rs
@@ -3,5 +3,12 @@ use super::gift_shop::GiftShop;
 pub fn run(raw_input: String) {
     let input: Vec<&str> = raw_input.split(",").collect();
     println!("Day2:");
-    println!("  Invalid ids: {}", GiftShop::add_up_invalid_ids(&input));
+    println!(
+        "  Sum of invalid ids: {}",
+        GiftShop::sum_of_id_repeated_twice(&input)
+    );
+    println!(
+        "  Sum of invalid ids repeated more than twice: {}",
+        GiftShop::sum_of_id_repeated_more_than_twice(&input)
+    );
 }
