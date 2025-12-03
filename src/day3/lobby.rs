5faba967fcee72e67056e806afe3338147406cf8
tomato2718 <qweasdzxc030@gmail.com>
2025-12-03 17:25:21 +0800

    Feat: Day3 q2

--- src/day3/lobby.rs
+++ src/day3/lobby.rs
@@ -1,30 +1,35 @@
 pub struct Lobby {}
 
 impl Lobby {
-    pub fn find_largest_joltage(banks: &Vec<&str>) -> usize {
-        banks.iter().map(|bank| Lobby::largest_joltage(*bank)).sum()
-    }
-
-    fn largest_joltage(bank: &str) -> usize {
-        bank.chars()
-            .rev()
-            .fold([None, None], |[max, second], battery| {
-                let b = battery
-                    .to_digit(10)
-                    .expect("Battery shoud contains only digits") as usize;
-                if second.is_none() {
-                    [None, Some(b)]
-                } else if max.is_none() {
-                    [Some(b), second]
-                } else if b >= max.unwrap() {
-                    [Some(b), max.max(second)]
-                } else {
-                    [max, second]
-                }
-            })
+    pub fn find_largest_joltage(banks: &Vec<&str>, batteries: usize) -> usize {
+        banks
             .iter()
-            .flatten()
-            .fold(0, |acc, cur| acc * 10 + cur)
+            .map(|bank| {
+                bank.chars()
+                    .map(|c| c.to_digit(10).expect("Battery shoud contains only digits") as usize)
+                    .rev()
+            })
+            .map(|mut bank| {
+                let mut res: Vec<usize> = bank.by_ref().take(batteries).collect();
+                res.reverse();
+
+                for battery in bank {
+                    if battery < res[0] {
+                        continue;
+                    };
+                    let mut i = 0;
+                    while i < batteries - 1 && res[i] >= res[i + 1] {
+                        i += 1;
+                    }
+                    for j in (1..=i).rev() {
+                        res[j] = res[j - 1];
+                    }
+                    res[0] = battery;
+                }
+
+                res.iter().fold(0, |acc, cur| acc * 10 + cur)
+            })
+            .sum()
     }
 }
 
@@ -33,7 +38,7 @@ mod test {
     use super::*;
 
     #[test]
-    fn find_largest_joltage() {
+    fn find_largest_joltage_with_2_batteries() {
         let banks = vec![
             "987654321111111",
             "811111111111119",
@@ -41,6 +46,18 @@ mod test {
             "818181911112111",
         ];
 
-        assert_eq!(Lobby::find_largest_joltage(&banks), 357);
+        assert_eq!(Lobby::find_largest_joltage(&banks, 2), 357);
+    }
+
+    #[test]
+    fn find_largest_joltage_with_12_batteries() {
+        let banks = vec![
+            "987654321111111",
+            "811111111111119",
+            "234234234234278",
+            "818181911112111",
+        ];
+
+        assert_eq!(Lobby::find_largest_joltage(&banks, 12), 3121910778619);
     }
 }
--- src/day3/run.rs
+++ src/day3/run.rs
@@ -3,5 +3,12 @@ use super::lobby::Lobby;
 pub fn run(raw_input: String) {
     let input: Vec<&str> = raw_input.split("\n").collect();
     println!("Day3:");
-    println!("  Largest joltage: {}", Lobby::find_largest_joltage(&input));
+    println!(
+        "  Largest joltage with 2 batteries: {}",
+        Lobby::find_largest_joltage(&input, 2)
+    );
+    println!(
+        "  Largest joltage with 12 batteries: {}",
+        Lobby::find_largest_joltage(&input, 12)
+    );
 }
