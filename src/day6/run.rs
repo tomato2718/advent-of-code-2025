fe0e5b05aba72fe33dc1218a55e7319d2dd0de76
tomato2718 <qweasdzxc030@gmail.com>
2025-12-08 16:01:56 +0800

    Feat: Day6 q2

--- src/day6/run.rs
+++ src/day6/run.rs
@@ -2,21 +2,18 @@ use super::trash_compactor::TrashCompactor;
 
 pub fn run(raw_input: String) {
     let input = raw_input.split("\n").collect::<Vec<&str>>();
-    let mut input_iter = input.iter().rev();
+    let mut input_iter = input.into_iter().rev();
 
     let signs: Vec<&str> = input_iter.next().unwrap().split_whitespace().collect();
-    let numbers: Vec<Vec<usize>> = input_iter
-        .map(|row| {
-            row.split_whitespace()
-                .map(|n| n.parse().expect("Numbers should contains only digits"))
-                .collect()
-        })
-        .rev()
-        .collect();
+    let numbers: Vec<&str> = input_iter.rev().collect();
 
     println!("Day6:");
     println!(
         "  Answer: {}",
         TrashCompactor::solve_math_work_sheet(&numbers, &signs)
+    );
+    println!(
+        "  Answer in correct format: {}",
+        TrashCompactor::solve_math_work_sheet_in_correct_format(&numbers, &signs)
     );
 }
--- src/day6/trash_compactor.rs
+++ src/day6/trash_compactor.rs
@@ -1,24 +1,61 @@
 pub struct TrashCompactor;
 
 impl TrashCompactor {
-    pub fn solve_math_work_sheet(numbers: &Vec<Vec<usize>>, signs: &Vec<&str>) -> usize {
+    pub fn solve_math_work_sheet(numbers: &Vec<&str>, signs: &Vec<&str>) -> usize {
+        let mut fixed = vec![Vec::new(); signs.len()];
         numbers
             .iter()
-            .zip(std::iter::repeat(signs))
-            .fold(
-                signs
-                    .iter()
-                    .map(|sign| if sign == &"+" { 0 } else { 1 })
-                    .collect::<Vec<usize>>(),
-                |acc, (number, sign)| {
-                    acc.into_iter()
-                        .zip(number)
-                        .zip(sign)
-                        .map(|((a, n), s)| if s == &"+" { a + n } else { a * n })
-                        .collect()
-                },
-            )
+            .map(|row| {
+                row.split_whitespace().map(|n| {
+                    n.parse::<usize>()
+                        .expect("Numbers should contains only digits")
+                })
+            })
+            .for_each(|number| {
+                number.enumerate().for_each(|(i, n)| fixed[i].push(n));
+            });
+
+        TrashCompactor::solve(&fixed, signs)
+    }
+
+    pub fn solve_math_work_sheet_in_correct_format(
+        numbers: &Vec<&str>,
+        signs: &Vec<&str>,
+    ) -> usize {
+        let mut fixed = Vec::new();
+        let mut cur = vec![];
+        for i in (0..numbers[0].len()).rev() {
+            let n = numbers.iter().fold(0, |acc, number| {
+                if let Ok(n) = number.get(i..=i).unwrap().parse::<usize>() {
+                    acc * 10 + n
+                } else {
+                    acc
+                }
+            });
+            if n == 0 {
+                fixed.push(cur);
+                cur = vec![];
+            } else {
+                cur.push(n);
+            }
+        }
+        fixed.push(cur);
+        fixed.reverse();
+
+        TrashCompactor::solve(&fixed, signs)
+    }
+
+    fn solve(numbers: &Vec<Vec<usize>>, signs: &Vec<&str>) -> usize {
+        numbers
             .iter()
+            .zip(signs)
+            .map(|(number, sign)| {
+                if sign == &"*" {
+                    number.iter().fold(1, |acc, cur| acc * cur)
+                } else {
+                    number.iter().fold(0, |acc, cur| acc + cur)
+                }
+            })
             .sum()
     }
 }
@@ -29,15 +66,21 @@ mod test {
 
     #[test]
     fn solve_math_work_sheet() {
-        let numbers: Vec<Vec<usize>> = vec![
-            vec![123, 328, 51, 64],
-            vec![45, 64, 387, 23],
-            vec![6, 98, 215, 314],
-        ];
+        let numbers: Vec<&str> = vec!["123 328  51 64 ", " 45 64  387 23 ", "  6 98  215 314"];
         let signs: Vec<&str> = vec!["*", "+", "*", "+"];
 
         let answer = TrashCompactor::solve_math_work_sheet(&numbers, &signs);
 
         assert_eq!(answer, 4277556);
+    }
+
+    #[test]
+    fn solve_math_work_sheet_in_correct_format() {
+        let numbers: Vec<&str> = vec!["123 328  51 64 ", " 45 64  387 23 ", "  6 98  215 314"];
+        let signs: Vec<&str> = vec!["*", "+", "*", "+"];
+
+        let answer = TrashCompactor::solve_math_work_sheet_in_correct_format(&numbers, &signs);
+
+        assert_eq!(answer, 3263827);
     }
 }
