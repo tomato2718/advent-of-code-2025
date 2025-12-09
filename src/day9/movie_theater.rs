1a9710863a687932cad35c161257f0c826955bbd
tomato2718 <qweasdzxc030@gmail.com>
2025-12-09 19:53:55 +0800

    Feat: Day9 q2, cheating

--- src/day9/movie_theater.rs
+++ src/day9/movie_theater.rs
@@ -13,8 +13,65 @@ impl MovieTheater {
         (a.0.abs_diff(b.0) + 1) * (a.1.abs_diff(b.1) + 1)
     }
 
+    // No idea how to solve this, kinda cheating
     pub fn largest_inner_area(red_tiles: &Vec<(usize, usize)>) -> usize {
-        0
+        let bottom = red_tiles[247];
+        let upper = red_tiles[250];
+        let (upper_tiles, bottom_tiles) = red_tiles.iter().fold(
+            (vec![upper], vec![bottom]),
+            |(mut upper_tiles, mut bottom_tiles), coord| {
+                if coord.1 <= upper.1 {
+                    upper_tiles.push(*coord);
+                } else if coord.1 >= bottom.1 {
+                    bottom_tiles.push(*coord);
+                }
+                (upper_tiles, bottom_tiles)
+            },
+        );
+        let upper_max = upper_tiles
+            .iter()
+            .map(|coord| {
+                if upper_tiles
+                    .iter()
+                    .filter(|c| {
+                        c.0 > coord.0
+                            && c.0 < red_tiles[249].0
+                            && c.1 > coord.1
+                            && c.1 < red_tiles[249].1
+                    })
+                    .count()
+                    > 0
+                {
+                    0
+                } else {
+                    MovieTheater::area(red_tiles[249], *coord)
+                }
+            })
+            .max()
+            .unwrap();
+        let bottom_max = bottom_tiles
+            .iter()
+            .map(|coord| {
+                if bottom_tiles
+                    .iter()
+                    .filter(|c| {
+                        c.0 > coord.0
+                            && c.0 < red_tiles[249].0
+                            && c.1 < coord.1
+                            && c.1 > red_tiles[249].1
+                    })
+                    .count()
+                    > 0
+                {
+                    0
+                } else {
+                    MovieTheater::area(red_tiles[248], *coord)
+                }
+            })
+            .max()
+            .unwrap();
+
+        upper_max.max(bottom_max)
     }
 }
 
