0e02af313805fb23e9abf4ed98f25532f222fa48
tomato2718 <qweasdzxc030@gmail.com>
2025-12-10 16:40:12 +0800

    Feat: Day10 q2 placeholder

--- src/day10/factory.rs
+++ src/day10/factory.rs
@@ -11,7 +11,7 @@ impl Factory {
         manual.iter().map(Factory::find_press).sum()
     }
 
-    fn find_press((lights, buttons, joltages): &(Lights, Buttons, Joltages)) -> usize {
+    fn find_press((lights, buttons, _): &(Lights, Buttons, Joltages)) -> usize {
         for comb in combine::from_vec(buttons) {
             if Factory::is_valid(lights, &comb) {
                 return comb.len();
@@ -28,6 +28,13 @@ impl Factory {
             }
         }
         &pressed == lights
+    }
+
+    pub fn fewest_press_met_joltage_requirement(
+        manual: &Vec<(Lights, Buttons, Joltages)>,
+    ) -> usize {
+        // TODO: Will need some math to solve this
+        0
     }
 }
 
@@ -76,5 +83,48 @@ mod test {
         let count = Factory::fewest_press(&inputs);
 
         assert_eq!(count, 7);
+    }
+
+    #[test]
+    fn fewest_press_met_joltage_requirement() {
+        let inputs = vec![
+            (
+                vec![false, true, true, false],
+                vec![
+                    vec![3],
+                    vec![1, 3],
+                    vec![2],
+                    vec![2, 3],
+                    vec![0, 2],
+                    vec![0, 1],
+                ],
+                vec![3, 5, 4, 7],
+            ),
+            (
+                vec![false, false, false, true, false],
+                vec![
+                    vec![0, 2, 3, 4],
+                    vec![2, 3],
+                    vec![0, 4],
+                    vec![0, 1, 2],
+                    vec![1, 2, 3, 4],
+                ],
+                vec![7, 5, 12, 7, 2],
+            ),
+            (
+                vec![false, true, true, true, false, true],
+                vec![
+                    vec![0, 1, 2, 3, 4],
+                    vec![0, 3, 4],
+                    vec![0, 1, 2, 4, 5],
+                    vec![1, 2],
+                ],
+                vec![10, 11, 11, 5, 10, 5],
+            ),
+        ];
+
+        let count = Factory::fewest_press_met_joltage_requirement(&inputs);
+
+        assert_eq!(count, 33);
     }
 }
